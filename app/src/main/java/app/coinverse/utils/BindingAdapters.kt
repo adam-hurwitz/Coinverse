762c699cadef79ab379e4f895199c6ebb3625cb5
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2018-11-09 11:12:09 -0800

    refactor: placeholder, error, preview content transparency

--- app/src/main/java/app/coinverse/utils/BindingAdapters.kt
+++ app/src/main/java/app/coinverse/utils/BindingAdapters.kt
@@ -3,6 +3,7 @@ package app.coinverse.utils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import app.coinverse.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions.circleCropTransform
@@ -13,6 +14,9 @@ fun ImageView.setImageUrl(url: String?) {
     GlideApp.with(context)
             .load(url)
             .transform(RoundedCorners(CONTENT_IMAGE_CORNER_RADIUS))
+            .placeholder(R.drawable.content_placeholder)
+            .error(R.drawable.coinverse_logo_placeholder)
+            .fallback(R.drawable.coinverse_logo_placeholder)
             .into(this)
 }
 
